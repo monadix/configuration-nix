66e395988ea3aa88130f93b2d10b5424b7e39c7e
chell4 <arsenijalfeev@gmail.com>
2024-06-01 14:30:39 +0300

    idk maybe it does smth

--- hardware-specific/chell-nixos.nix
+++ hardware-specific/chell-nixos.nix
@@ -89,8 +89,19 @@
     cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
     opengl = {
       enable = true;
+      
       driSupport = true;
       driSupport32Bit = true;
+
+      extraPackages = with pkgs; [
+	      vaapiVdpau
+	      libvdpau-va-gl
+      ];
+
+      extraPackages32 = with pkgs.driversi686Linux; [
+	      vaapiVdpau
+	      libvdpau-va-gl
+      ];
     };
     
     nvidia = {
