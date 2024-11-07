3f978076b8c6a4a3d0097dd4e9e6d10f28c39bf7
chell4 <arsenijalfeev@gmail.com>
2024-11-07 17:17:58 +0300

    system-wide postgres was a bad idea

--- configuration.nix
+++ configuration.nix
@@ -172,11 +172,6 @@
     authentication = pkgs.lib.mkOverride 10 ''
       #type database DBuser auth-method
       local all      all    trust
-      host  all      all     127.0.0.1/32   trust
-      host  all      all     ::1/128        trust
-    '';
-    initialScript = pkgs.writeText "backend-initScript" ''
-      GRANT ALL PRIVILEGES TO postgres;
     '';
   };
 
