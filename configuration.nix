8a261b8682c987ccd3714c659351a0ae27ff3fff
monadix <arsenijalfeev@gmail.com>
2025-07-15 10:46:05 +0300

    feat: enable garnix binary cache substituter

--- configuration.nix
+++ configuration.nix
@@ -131,7 +131,19 @@
   nix = {
     package = pkgs.nixVersions.latest;
 
-    settings.experimental-features = ["nix-command" "flakes"];
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      
+      substituters = [
+        "https://cache.nixos.org/"
+        "https://cache.garnix.io"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      ];
+    };
   };
 
   users.users.chell = {
