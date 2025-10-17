dc714a1545256cec26adb8280fe97fa181e9be57
monadix <arsenijalfeev@gmail.com>
2025-10-17 10:49:54 +0300

    fix(thinkpad): remove deprecated amdvlk driver

--- hardware-specific/chell-thinkpad.nix
+++ hardware-specific/chell-thinkpad.nix
@@ -70,12 +70,10 @@
       enable32Bit = true;
 
       extraPackages = with pkgs; [
-        amdvlk
 	      libvdpau-va-gl
       ];
 
       extraPackages32 = with pkgs.driversi686Linux; [
-        amdvlk
 	      libvdpau-va-gl
       ];
     };
