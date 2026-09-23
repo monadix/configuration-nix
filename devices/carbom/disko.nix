a9cb0f2e01f1363c4ea69961db12591497bb5b0c
Alfeev Arsenii <111121445+monadix@users.noreply.github.com>
2026-09-23 18:14:29 +0300

    Add encrypted Disko configuration for carbom ThinkPad

---
+++ devices/carbom/disko.nix
@@ -0,0 +1,48 @@
+{
+  disko.devices.disk.main = {
+    type = "disk";
+    # Check this against lsblk before running Disko; it erases the whole disk.
+    device = "/dev/nvme0n1";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          size = "1G";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+            mountOptions = [ "umask=0077" ];
+          };
+        };
+        root = {
+          size = "100%";
+          content = {
+            type = "luks";
+            name = "cryptroot";
+            settings.allowDiscards = true;
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "root" = {
+                  mountpoint = "/";
+                  mountOptions = [ "compress=zstd" ];
+                };
+                "home" = {
+                  mountpoint = "/home";
+                  mountOptions = [ "compress=zstd" ];
+                };
+                "nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [ "compress=zstd" "noatime" ];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
